3bc26af89647405ed2b64a705460f92bf30c4732
Stealth1226 <rishawniyer06@gmail.com>
2021-09-13 09:12:54 +0530

    msm8996-common: init: update fingerprint to redfin september

--- init/init_lge-msm8996.cpp
+++ init/init_lge-msm8996.cpp
@@ -94,6 +94,6 @@ static void set_build_description(const char *description){
 void vendor_load_properties()
 {
     workaround_snet_properties();
-    set_build_fingerprint("google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys");
-    set_build_description("redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys");
+    set_build_fingerprint("google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys");
+    set_build_description("redfin-user 11 RQ3A.210905.001 7511028 release-keys");
 }
