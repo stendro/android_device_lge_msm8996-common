2dc9eeb09b9e395cb4845176b5cb34e33f7d54f2
stendro <33956801+stendro@users.noreply.github.com>
2022-07-10 19:59:56 +0200

    BoardConfigCommon: Set selinux = permissive for develop

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,8 +48,9 @@ TARGET_NO_BOOTLOADER := true
 HWUI_COMPILE_FOR_PERF := true
 
 # Kernel
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x02000000
